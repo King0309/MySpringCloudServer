d68798c9e4750f553cba131b758cbd4f8cee0893
king <rainandroid@163.com>
2018-12-18 09:41:27 +0800

    My spring cloud Server code

---
+++ SpringCloudServer/src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package springcloud.helloworld.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author king
+ * Eureka Server
+ * 注册服务中心
+ * 2018-12-18
+ * */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
