99bc48819b44438284664bb90dd41d4eba7e25a6
King0309 <42104465+King0309@users.noreply.github.com>
2019-08-19 15:18:39 +0800

    增加日志打印
    增加了一条打印，做测试！

--- SpringCloudServer/src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
+++ SpringCloudServer/src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
@@ -17,5 +17,6 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println("success");
     }
 }
